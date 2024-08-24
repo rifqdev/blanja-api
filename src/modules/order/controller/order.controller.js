b4d5383242d348712eba7e10090a421233eb7519
rifqiay <muhammadrifqiay5@gmail.com>
2024-08-24 13:27:05 +0700

    add field product name and photo when create order

--- src/modules/order/controller/order.controller.js
+++ src/modules/order/controller/order.controller.js
@@ -5,6 +5,59 @@ const Products = require("../../products/model/products.model");
 const wrapper = require("../../../helper/responses");
 const { Op } = require("sequelize");
 
+// const createOrder = async (req, res) => {
+//   try {
+//     const userId = req.user.userId;
+//     const payload = { ...req.body };
+//     payload.custommer_id = userId;
+
+//     const getAddress = await Address.findOne({ where: { user_id: userId, primary_address: true } });
+//     if (!getAddress) throw new Error("failed create checkout please add address first!");
+
+//     const getCart = await Cart.findAll({ where: { custommer_id: userId } });
+//     if (!getCart) throw new Error("cart is empty");
+//     payload.products = getCart.map((item) => {
+//       return item.dataValues;
+//     });
+
+//     const groupedBySeller = getCart.reduce((acc, cartItem) => {
+//       const sellerId = cartItem.dataValues.seller_id;
+//       if (!acc[sellerId]) {
+//         acc[sellerId] = [];
+//       }
+//       acc[sellerId].push(cartItem.dataValues);
+//       return acc;
+//     }, {});
+
+//     const groupedBySellerArray = Object.values(groupedBySeller).map((group, index) => {
+//       return {
+//         id: index + 1,
+//         items: group,
+//       };
+//     });
+
+//     let result;
+//     for (const product of groupedBySellerArray) {
+//       payload.custommer_address = getAddress.dataValues;
+//       payload.products = product.items;
+//       payload.seller_id = product.items[0].seller_id;
+
+//       result = await Order.create(payload);
+//       if (!result) throw new Error("failed create order please contact the developer");
+
+//       for (const x of payload.products) {
+//         const getProduct = await Products.findOne({ where: { id: x.product_id } });
+//         await Products.update({ stock: getProduct.dataValues.stock - 1 }, { where: { id: x.product_id } });
+//       }
+//     }
+
+//     await Cart.destroy({ where: { custommer_id: userId } });
+//     return wrapper.sendSuccessResponse(res, "order successfully", result, 200);
+//   } catch (error) {
+//     return wrapper.sendErrorResponse(res, "order failed", error.message, 500);
+//   }
+// };
+
 const createOrder = async (req, res) => {
   try {
     const userId = req.user.userId;
@@ -16,16 +69,22 @@ const createOrder = async (req, res) => {
 
     const getCart = await Cart.findAll({ where: { custommer_id: userId } });
     if (!getCart) throw new Error("cart is empty");
-    payload.products = getCart.map((item) => {
-      return item.dataValues;
-    });
 
-    const groupedBySeller = getCart.reduce((acc, cartItem) => {
-      const sellerId = cartItem.dataValues.seller_id;
+    payload.products = await Promise.all(getCart.map(async (item) => {
+      const product = await Products.findOne({ where: { id: item.product_id } });
+      return {
+        ...item.dataValues,
+        product_name: product.name,
+        product_photo: product.photo,
+      };
+    }));
+
+    const groupedBySeller = payload.products.reduce((acc, cartItem) => {
+      const sellerId = cartItem.seller_id;
       if (!acc[sellerId]) {
         acc[sellerId] = [];
       }
-      acc[sellerId].push(cartItem.dataValues);
+      acc[sellerId].push(cartItem);
       return acc;
     }, {});
 
@@ -47,7 +106,7 @@ const createOrder = async (req, res) => {
 
       for (const x of payload.products) {
         const getProduct = await Products.findOne({ where: { id: x.product_id } });
-        await Products.update({ stock: getProduct.dataValues.stock - 1 }, { where: { id: x.product_id } });
+        await Products.update({ stock: getProduct.dataValues.stock - x.quantity }, { where: { id: x.product_id } });
       }
     }
 
