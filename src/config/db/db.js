738ec5cde56123e93b9b22d03024438fbd4ec699
rifq <mrifqiay@gmail.com>
2024-10-02 06:52:48 +0700

    update config db

--- src/config/db/db.js
+++ src/config/db/db.js
@@ -1,9 +1,10 @@
 const { Sequelize, DataTypes } = require("sequelize");
 const NODE_ENV = process.env.NODE_ENV;
 const config = require("../db/config")[NODE_ENV];
-
+console.log(config)
 const sequelize = new Sequelize(config.database, config.username, config.password, {
   host: config.host,
+  port: config.port,
   dialect: config.dialect,
   logging: false,
 });
